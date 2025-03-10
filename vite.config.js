b5efe82dbc29fc02c84bc78aba71988544e97df2
David <olayemi289@gmail.com>
2025-03-10 20:24:55 +0100

    fix: corrected error for vite asset handling by importing images in app.js

--- vite.config.js
+++ vite.config.js
@@ -10,10 +10,4 @@ export default defineConfig({
         }),
         tailwindcss(),
     ],
-    build: {
-        manifest: true,
-        rollupOptions: {
-            input: 'resources/js/app.js',
-        },
-    },
 });
